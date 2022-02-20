7faf5013b3dcd3c575b59ccbb63f8cd294e49e80
MrAnyx <1ptitigre@gmail.com>
2022-02-20 01:02:13 +0100

    ✨ 🎉 Created Symfony project + added lit + added encore + added swup

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/page1', name: 'app_page1')]
+    public function page1(): Response
+    {
+        return $this->render('home/page1.html.twig');
+    }
+}
